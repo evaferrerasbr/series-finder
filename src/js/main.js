070fda49ea7c42b4fe1af7cb669efe2e5f50c3a5
evaferrerasbr <evaferrerasbr@gmail.com>
2020-10-23 15:15:40 +0200

    Edit getFavorits() for including an object into favorites array

--- src/js/main.js
+++ src/js/main.js
@@ -2,9 +2,12 @@
 
 const button = document.querySelector('.js-btn');
 const results = document.querySelector('.js-list--results');
+const favoriteList = document.querySelector('.js-list--favorites');
 let search = '';
 let shows = [];
 let favoriteShows = [];
+let favoriteId = [];
+let liElement;
 
 function handlerEvent() {
   shows = [];
@@ -13,7 +16,7 @@ function handlerEvent() {
 }
 
 function searchShows() {
-  fetch(`http://api.tvmaze.com/search/shows?q=${search}`)
+  fetch(`//api.tvmaze.com/search/shows?q=${search}`)
     .then((response) => response.json())
     .then((data) => {
       for (let i = 0; i < data.length; i++) {
@@ -27,13 +30,9 @@ function searchShows() {
 function paintShows() {
   results.innerHTML = '';
   for (let i = 0; i < shows.length; i++) {
-    const liElement = document.createElement('li');
+    liElement = document.createElement('li');
     liElement.classList.add('list--item', 'js-list--item');
-    liElement.setAttribute('id', i);
-    if (favoriteShows.indexOf(parseInt(liElement.id)) !== -1) {
-      liElement.classList.add('favorite');
-    }
-    results.appendChild(liElement);
+    liElement.setAttribute('id', shows[i].id);
     const imgElement = document.createElement('img');
     if (shows[i].image) {
       imgElement.setAttribute('src', shows[i].image.medium);
@@ -49,14 +48,44 @@ function paintShows() {
     const titleContent = document.createTextNode(`${shows[i].name}`);
     titleElement.appendChild(titleContent);
     liElement.appendChild(titleElement);
+    results.appendChild(liElement);
+  }
+  paintFavorite();
+}
+
+function paintFavorite() {
+  for (let i = 0; i < favoriteShows.length; i++) {
+    favoriteList.innerHTML += `<li><img src="${favoriteShows[i].image}" class="img"/><h3>${favoriteShows[i].name}</h3></li>`;
+
+    // const liFav = document.createElement('li');
+    // const imgFav = document.createElement('img');
+    // imgFav.src = favoriteShows[i].image;
+    // imgFav.classList.add('img');
+    // liFav.appendChild(imgFav);
+    // const titleFav = document.createElement('h3');
+    // const titleFavContent = document.createTextNode(`${favoriteShows[i].name}`);
+    // titleFav.appendChild(titleFavContent);
+    // liFav.appendChild(titleFav);
+    // favoriteList.appendChild(liFav);
   }
 }
 
 function getFavorites(event) {
+  const current = event.currentTarget;
+  let imgCurrent = current.querySelector('.img');
+  let titleCurrent = current.querySelector('h3');
+  const objectFavorite = {
+    name: titleCurrent.innerHTML,
+    image: imgCurrent.src,
+    id: event.currentTarget.id,
+  };
   const selectedShow = parseInt(event.currentTarget.id);
-  const indFavorite = favoriteShows.indexOf(selectedShow);
+  favoriteId = favoriteShows.map(function (element) {
+    return parseInt(element.id);
+  }); //creamos un nuevo array a partir del array de favoritos que solo contenga los id para poder buscar en el indexOf
+  const indFavorite = favoriteId.indexOf(selectedShow);
   if (indFavorite === -1) {
-    favoriteShows.push(selectedShow);
+    favoriteShows.push(objectFavorite);
   } else {
     favoriteShows.splice(indFavorite, 1);
   }
