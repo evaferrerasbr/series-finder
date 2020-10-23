82aa4bb19db53ffc265a4c4a2b684a2e4a667cf9
evaferrerasbr <evaferrerasbr@gmail.com>
2020-10-23 09:22:27 +0200

    Add listeners to all list elements and create function to add favorites to a new array

--- src/js/main.js
+++ src/js/main.js
@@ -4,6 +4,7 @@ const button = document.querySelector('.js-btn');
 const results = document.querySelector('.js-list');
 let search = '';
 let shows = [];
+let favoriteShows = [];
 
 function handlerEvent() {
   shows = [];
@@ -19,6 +20,7 @@ function searchShows() {
         shows.push(data[i].show);
       }
       paintShows();
+      listenSearch();
     });
 }
 
@@ -26,9 +28,9 @@ function paintShows() {
   results.innerHTML = '';
   for (let i = 0; i < shows.length; i++) {
     const liElement = document.createElement('li');
+    liElement.classList.add('list--item', 'js-list--item');
+    liElement.setAttribute('id', i);
     results.appendChild(liElement);
-    const divElement = document.createElement('div');
-    liElement.appendChild(divElement);
     const imgElement = document.createElement('img');
     if (shows[i].image) {
       imgElement.setAttribute('src', shows[i].image.medium);
@@ -38,11 +40,29 @@ function paintShows() {
         'https://via.placeholder.com/210x295/ffffff/666666/?text=TV'
       );
     }
-    divElement.appendChild(imgElement);
+    liElement.appendChild(imgElement);
     const titleElement = document.createElement('h3');
     const titleContent = document.createTextNode(`${shows[i].name}`);
     titleElement.appendChild(titleContent);
-    divElement.appendChild(titleElement);
+    liElement.appendChild(titleElement);
+  }
+}
+
+function getFavorites(event) {
+  const selectedShow = parseInt(event.currentTarget.id);
+  const indFavorite = favoriteShows.indexOf(selectedShow);
+  console.log(indFavorite);
+  if (indFavorite === -1) {
+    favoriteShows.push(selectedShow);
+  } else {
+    favoriteShows.splice(indFavorite, 1);
+  }
+}
+
+function listenSearch() {
+  const liItems = document.querySelectorAll('.js-list--item');
+  for (const liItem of liItems) {
+    liItem.addEventListener('click', getFavorites);
   }
 }
 
