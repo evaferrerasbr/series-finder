6d0508c67273d83dca73ff4338c8edb9bbeebb8e
evaferrerasbr <evaferrerasbr@gmail.com>
2020-11-01 10:46:52 +0100

    Add commit to merge

---
+++ src/js/4localstorage.js
@@ -0,0 +1,27 @@
+'use strict';
+
+//LOCAL STORAGE
+//saves the favorites array in local storage
+function setLocalStorage() {
+  const setStringData = JSON.stringify(favoriteShows);
+  localStorage.setItem('favorites', setStringData);
+}
+
+//gets the info from local storage if it's not empty
+function getLocalStorage() {
+  const getStringData = localStorage.getItem('favorites');
+  savedFavorites = JSON.parse(getStringData);
+  if (savedFavorites === null) {
+    listenSearch();
+  } else {
+    favoriteShows = savedFavorites;
+    paintFavorite();
+    listenSearch();
+  }
+  if (favoriteShows.length === 0) {
+    favoritesList.innerHTML = '';
+    notFavoritesYet();
+  }
+}
+
+getLocalStorage();
